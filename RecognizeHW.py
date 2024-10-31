3a963f4ddc68f1c7bc92fa17d5bd5b6f9b949df4
YNZL0702 <96977832+YNZL0702@users.noreply.github.com>
2024-10-31 21:58:38 +0800

    Update RecognizeHW.py

--- RecognizeHW.py
+++ RecognizeHW.py
@@ -9,7 +9,6 @@ from tkinter import filedialog
 import re
 
 
-
 '''''''''
 目标区域的识别
 '''''''''
@@ -22,55 +21,47 @@ def Find_LIMS_No_Contours(imagePath):
     roi=img[300:400, 410:890]
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
-    # 参数：输入矩阵、输出矩阵数据类型、设置1、0时差分方向为水平x方向的核卷积，设置0、1为垂直y方向,ksize：核的尺寸;CV_8U 无符号 8 位整数
     sobel_1x = cv2.Sobel(gray, cv2.CV_8U, 1, 0, ksize = 3)   
     sobel_1y = cv2.Sobel(gray, cv2.CV_8U, 0, 1, ksize = 3)
     combined_sobel=cv2.bitwise_and(sobel_1x, sobel_1y)
     
     blur=cv2.GaussianBlur(combined_sobel,(9,9),0)
 
-    #  二值化, Otsu阈值法会自动计算阈值。
     ret, binary = cv2.threshold(blur , 0, 255, cv2.THRESH_OTSU+cv2.THRESH_BINARY)
 
-    # 设置膨胀和腐蚀操作的核函数
+
     element1 = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 3))
     element2 = cv2.getStructuringElement(cv2.MORPH_RECT, (24, 6))
-    #膨胀一次，让轮廓突出
+
     dilation = cv2.dilate(binary, element2, iterations = 1)
 
-    # 腐蚀一次，去掉细节，如表格线等。注意这里去掉的是竖直的线
     erosion = cv2.erode(dilation, element1, iterations = 1)
 
-    #再次膨胀，让轮廓明显一些
     dilation2 = cv2.dilate(erosion, element2, iterations = 3)
 
-    #查找和筛选文字区域
-
     region = []
-    #  查找轮廓
+    
     contours, hierarchy = cv2.findContours(dilation2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # 利用以上函数可以得到多个轮廓区域，存在一个列表中。
+
 
     for i in range(len(contours)):
         # 遍历所有轮廓
         
         cnt = contours[i]
 
-        # 计算该轮廓的面积
         area = cv2.contourArea(cnt)
-
-        # 面积小的都筛选掉、筛选出目标区域
+        
         if(area < 4000):
-            continue  # 跳出本次 i 循环
+            continue  
 
-        # 找到最小的矩形，该矩形可能有方向
+      
         rect = cv2.minAreaRect(cnt)
  
         # box是四个点的坐标
         box = cv2.boxPoints(rect)
         box = np.int0(box)
 
-        # 计算高和宽
+     
         height = abs(box[0][1] - box[2][1])
         width = abs(box[0][0] - box[2][0])
 
@@ -93,7 +84,7 @@ def Find_LIMS_No_Contours(imagePath):
 
         crop_img: object = roi[y1:y1 + hight, x1:x1 + width]
 
-        #* 转为 base64 (base64是一个编码方式，用于在网络上传输数据)
+        #* 转为 base64 
         crop_img_encode=cv2.imencode('.jpg',crop_img)[1]
         crop_img_base64=str(base64.b64encode(crop_img_encode))[2:-1]   
         return crop_img_base64
@@ -103,7 +94,7 @@ def Find_LIMS_No_Contours(imagePath):
 手写文字识别
 '''''''''
 def Handwriting_recognition (img_base64):      
-    #输入 base64编码的图片
+  
     try:
     
         request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/handwriting"
@@ -132,22 +123,20 @@ def Handwriting_recognition (img_base64):
 将PDF的每一页转为图片
 '''''''''
 def PDF_to_JPG(PDF_Path):
-    img_paths = []                    # 图片路径列表
+    img_paths = []                    
 
     pdf = fitz.open(PDF_Path) 
 
     for i,pg in enumerate(range(0, pdf.pageCount)):
-        page = pdf[pg]                                                     # 获得每一页的对象
+        page = pdf[pg]                                                     
         trans = fitz.Matrix(3.0, 3.0).preRotate(0)                          
-        pm = page.getPixmap(matrix=trans, alpha=False)                      # 获得每一页的流对象
-        # pm.writePNG(dir_name + os.sep + base_name[:-4] + '_' + '{:0>3d}.png'.format(pg + 1))  # 保存图片
+        pm = page.getPixmap(matrix=trans, alpha=False)                      
+        # pm.writePNG(dir_name + os.sep + base_name[:-4] + '_' + '{:0>3d}.png'.format(pg + 1))  
         img_path = PDF_Path[:-4] + '_' + str(pg+1) + '.jpg'
-        pm.writePNG(img_path)                                           # 保存图片
+        pm.writePNG(img_path)                                         
         img_paths.append(img_path)
     pdf.close()
     return img_paths
-
-
 
 
 '''''''''
@@ -179,27 +168,27 @@ def same_File_Name(text_Name, Name_List):
 '''''''''
 识别后的文字，重命名图片
 '''''''''
-def ReName_IMG(PDF_Path):                          # 文件夹下所有文件名称重命名
+def ReName_IMG(PDF_Path):                          
       
-    _folder_Path=os.path.dirname(PDF_Path)        # 获取 文件夹 路径
-    os.chdir(_folder_Path)                         #切换到文件夹目录; 改变当前工作目录到指定的路径
+    _folder_Path=os.path.dirname(PDF_Path)      
+    os.chdir(_folder_Path)                       
   
-    # 所需要的图片 路径 列表
+  
     _images_Path=PDF_to_JPG(PDF_Path)             
 
-    # 获取 文件夹下所有文件名称
+
     _all_file_Names=os.listdir(_folder_Path)      
 
     for _file_path in _images_Path:
 
         _file_oldName=os.path.basename(_file_path)
 
-        #判断_file_oldName 是否为图片
+    
         if _file_oldName.endswith('.jpg'):          
 
             _Lims_Contour=Find_LIMS_No_Contours(_file_path)
 
-            # 延时0.5秒，防止百度API调用次数超限
+            # 延时0.5秒
             time.sleep(0.5)  
 
             _reg_Txt=Handwriting_recognition(_Lims_Contour)
